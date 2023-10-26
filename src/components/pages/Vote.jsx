577e2c8633e05cd540e5e99c463f43b3930c76d2
AyKira <62882339+AyKira@users.noreply.github.com>
2023-10-26 19:39:18 +0200

    funguje axios ale rexus a thunk ne

--- src/components/pages/Vote.jsx
+++ src/components/pages/Vote.jsx
@@ -1,12 +1,8 @@
-import React from "react";
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { useDispatch } from 'react-redux';
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
-import image1 from "../images/image1.jpg";
-import image2 from "../images/image2.jpg";
-import image3 from "../images/image3.jpg";
-import image4 from "../images/image1.jpg";
-export const pictures = [image1, image2, image3, image4];
+import axios from 'axios';
 
 const styles = {
   buttons1: {
@@ -25,6 +21,36 @@ const styles = {
 
 function Vote() {
   const dispatch = useDispatch();
+  const [imageURL, setImageURL] = useState('');
+
+  const fetchRandomCatImage = async () => {
+    try {
+      const response = await axios.get(
+        'https://api.thecatapi.com/v1/images/search?limit=1&size=full&sub_id=demo-93913c'
+      );
+      if (response.data.length > 0) {
+        // Extract the image URL from the response
+        const imageUrl = response.data[0].url;
+        setImageURL(imageUrl);
+      }
+    } catch (error) {
+      console.error('Error fetching cat image:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRandomCatImage(); // Fetch a random cat image when the component mounts
+  }, []);
+
+  const handleLoveIt = () => {
+    fetchRandomCatImage(); // Fetch a new random cat image when "Love it!" is clicked
+    // Handle the "Love it!" action (e.g., dispatch an action)
+  };
+
+  const handleNopeIt = () => {
+    fetchRandomCatImage(); // Fetch a new random cat image when "Nope it!" is clicked
+    // Handle the "Nope it!" action (e.g., dispatch an action)
+  };
 
   return (
     <Grid container spacing={2} columns={2} justifyContent="center" style={{ marginTop: '20px' }}>
@@ -32,7 +58,7 @@ function Vote() {
         <Button
           variant="contained"
           style={styles.buttons1}
-          onClick={() => { }}
+          onClick={handleLoveIt}
         >
           Love it!
         </Button>
@@ -41,15 +67,15 @@ function Vote() {
         <Button
           variant="contained"
           style={styles.buttons2}
-          onClick={() => { }}
+          onClick={handleNopeIt}
         >
           Nope it!
         </Button>
       </Grid>
       <Grid item xs={2}>
         <img
-          src={pictures[0]}
-          alt="Current Picture"
+          src={imageURL}
+          alt="Random Cat"
           style={styles.picture}
         />
       </Grid>
