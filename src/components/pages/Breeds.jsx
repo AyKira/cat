55009880a4b8231eeaab88e15101dbf645f31f01
AyKira <62882339+AyKira@users.noreply.github.com>
2023-11-23 22:52:58 +0100

    selector mui

--- src/components/pages/Breeds.jsx
+++ src/components/pages/Breeds.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import Grid from "@mui/material/Grid";
+import FormControl from '@mui/material/FormControl';
+import NativeSelect from '@mui/material/NativeSelect';
+import InputLabel from '@mui/material/InputLabel';
 
 function Breeds() {
-  return <div>Breeds Page</div>;
+  return (
+    <Grid container spacing={2} justifyContent="center" alignItems="center" sx={{ marginTop: '20px' }}>
+      <Grid item xs={12} md={4}>
+        <FormControl sx={{ width: '100%' }}>
+          <InputLabel variant="standard" htmlFor="uncontrolled-native">
+            Age
+          </InputLabel>
+          <NativeSelect
+            defaultValue={30}
+            inputProps={{
+              name: 'age',
+              id: 'uncontrolled-native',
+            }}
+          >
+            <option value={10}>Ten</option>
+            <option value={20}>Twenty</option>
+            <option value={30}>Thirty</option>
+          </NativeSelect>
+        </FormControl>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Breeds;
