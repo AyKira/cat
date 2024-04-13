9c62932fb62fcd7152fefb6b8939c512bc6103de
AyKira <AyKira@seznam.cz>
2024-04-13 18:02:36 +0200

    fix na reducery

--- src/redux-modules/breedSlice.js
+++ src/redux-modules/breedSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const initialState = {
@@ -7,54 +7,74 @@ const initialState = {
   breedName: '',
   breedImage: '',
   breedDescription: '',
-  breedImages: [],  
+  breedImages: [],
 };
+
+// list of breeds
+export const fetchBreeds = createAsyncThunk(
+  'breeds/fetchBreeds',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get('https://api.thecatapi.com/v1/breeds');
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+// pictures and details of a cat
+export const fetchBreedDetails = createAsyncThunk(
+  'breeds/fetchBreedDetails',
+  async (breedId, { getState, rejectWithValue }) => {
+    try {
+      const selectedBreed = breedId || getState().breeds.selectedBreed;
+      const response = await axios.get(`https://api.thecatapi.com/v1/images/search?limit=8&breed_id=${selectedBreed}`);
+      return response.data.map(img => img.url);
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
 
 export const breedSlice = createSlice({
   name: 'breeds',
   initialState,
   reducers: {
-    setBreeds: (state, action) => {
-      state.breeds = action.payload;
-    },
     setSelectedBreed: (state, action) => {
       state.selectedBreed = action.payload;
     },
-    setBreedDetails: (state, action) => {
+  },
+  extraReducers: {
+    [fetchBreeds.pending]: (state) => {
+      state.loading = true;
+    },
+    [fetchBreeds.fulfilled]: (state, action) => {
+      state.breeds = action.payload;
+      state.loading = false;
+    },
+    [fetchBreeds.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    [fetchBreedDetails.pending]: (state) => {
+      state.loading = true;
+    },
+    [fetchBreedDetails.fulfilled]: (state, action) => {
       const breed = state.breeds.find(b => b.id === state.selectedBreed);
       state.breedName = breed?.name || '';
       state.breedDescription = breed?.description || '';
-      state.breedImages = action.payload || []; 
+      state.breedImages = action.payload;
+      state.loading = false;
     },
-  },
+    [fetchBreedDetails.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  }
 });
 
-export const { setBreeds, setSelectedBreed, setBreedDetails } = breedSlice.actions;
-
-export const fetchBreeds = () => async (dispatch) => {
-  try {
-    const response = await axios.get('https://api.thecatapi.com/v1/breeds');
-    dispatch(setBreeds(response.data));
-    return Promise.resolve();
-  } catch (error) {
-    console.error('Error fetching breeds:', error);
-    return Promise.reject();
-  }
-};
-
-export const fetchBreedDetails = (breedId) => async (dispatch, getState) => {
-  try {
-    const state = getState();
-    const selectedBreed = breedId || state.breeds.selectedBreed;
-    
-    if (selectedBreed) {
-      const imageResponse = await axios.get(`https://api.thecatapi.com/v1/images/search?limit=8&breed_id=${selectedBreed}`);
-      dispatch(setBreedDetails(imageResponse.data.map(img => img.url)));  
-    }
-  } catch (error) {
-    console.error('Error fetching breed details:', error);
-  }
-};
+export const { setSelectedBreed } = breedSlice.actions;
 
 export const selectBreeds = (state) => state.breeds;
 
