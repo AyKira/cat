dc271f0eb052d86e7d43a6ac089ab6b99c000fd6
AyKira <AyKira@seznam.cz>
2024-04-13 16:02:54 +0200

    oprava na home a favorites page

--- src/components/pages/Favorites.jsx
+++ src/components/pages/Favorites.jsx
@@ -3,32 +3,27 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Grid from "@mui/material/Grid";
+import Typography from '@mui/material/Typography';
 
 const styles = {
-  container: {
-    marginTop: '20px',
-  },
-  picture: {
-    display: 'block',
-    margin: '0 5px',
-  },
+  picture: { width: '150px', height: '150px', objectFit: 'cover' },
+  text: { textAlign: 'center', margin: '20px 20px' },
 };
 
 function Favorites() {
   const savedUrls = useSelector((state) => state.randomPicture.savedUrls);
 
   return (
-    <Grid container spacing={2} justifyContent="center" style={styles.container}>
-      {savedUrls.map((imageUrl, index) => (
-        <Grid item key={index}>
-          <img
-            src={imageUrl}
-            alt={`Favorite ${index + 1}`}
-            style={{ width: '150px', height: '150px', objectFit: 'cover' }}
-          />
-        </Grid>
-      ))}
-    </Grid>
+    <>
+      <Typography variant="h3" style={styles.text}>Your liked pictures: </Typography>
+      <Grid container spacing={2} justifyContent="center" >
+        {savedUrls.map((imageUrl, index) => (
+          <Grid item key={index}>
+            <img src={imageUrl} style={styles.picture} />
+          </Grid>
+        ))}
+      </Grid>
+    </>
   );
 }
 
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
 
 function Home() {
   const style = {
     display: 'flex',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
     height: '80vh',
   };
+  const h1Style = {
+    marginBottom: '100px',
+  };
 
   return (
     <div style={style}>
-      <h2>Welcome to the world of CATOOOO!!!</h2>
+      <Typography variant="h3" style={h1Style}>
+        Welcome to the world of CATOOOO!!!
+      </Typography>
+      <Typography variant="h6">
+        Click on cat face to continue
+      </Typography>
     </div>
   );
 }
