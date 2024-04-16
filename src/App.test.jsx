eb78049b9043b09ff02b09b50f59fba8683e8830
markup <markup@2n.com>
2024-04-16 17:43:09 +0200

    tests from petřižál

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,47 +5,53 @@ import App from "./App";
 import "@testing-library/jest-dom";
 import { MemoryRouter } from 'react-router-dom';
 
+jest.mock('./components/SideBar/Sidebar', () => ({ isOpen, toggleDrawer }) => (<div data-testid="sideBarMock" onClick={toggleDrawer}>{'SideBarMockDiv ' + isOpen.toString()}</div>))
+
+describe('component App', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('component SideBar is not displayed when rendered', () => {
+    render(<App />);
+
+    expect(screen.getByText('SideBarMockDiv false')).toBeInTheDocument();
+  });
+
+  test('component SideBar has prop toggleDrawer toggling isOpen', async () => {
+    const user = userEvent.setup();
+    render(<App />);
+
+    await user.click(screen.getByTestId('sideBarMock'));
+    expect(screen.getByText('SideBarMockDiv true')).toBeInTheDocument();
+    await user.click(screen.getByTestId('sideBarMock'));
+    expect(screen.getByText('SideBarMockDiv false')).toBeInTheDocument();
+  });
+
+  test('Drawer toggles visibility correctly', async () => {
+    const user = userEvent.setup();
+    render(<App />);
+
+    screen.debug();
+  });
+
+  // App.test.jsx
 
 
+  test('navigates to /vote', async () => {
+    const user = userEvent.setup();
+
+    //nechapu pro4 se to to tadz sekne na provideru
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <App useInternalRouter={false} />
+      </MemoryRouter>
+    );
+
+    await user.click(screen.getByTestId('app-bar'));
+    await user.click(screen.getByTestId('VOTE'));
 
 
-test('Drawer toggles visibility correctly', async () => {
-  const user = userEvent.setup();
-  const { debug } = render(<Website />);
-
-  // Open the Drawer
-  await user.click(screen.getByTestId('app-bar'));
-  expect(screen.getByTestId('side-bar')).not.toHaveAttribute('aria-hidden', 'true');
-
-
-  // Close the Drawer
-  debug(); // tohle je bullshit proč se drawner neschova ale je misto toho hidden
-  await user.click(screen.getByTestId('app-bar'));
-  expect(screen.getByTestId('side-bar')).toHaveAttribute('aria-hidden', 'true');
-
-  // Open the Drawer last time
-  await user.click(screen.getByTestId('app-bar'));
-  expect(screen.getByTestId('side-bar')).not.toHaveAttribute('aria-hidden', 'true');
-
+    expect(screen.getByText('SAVE IT!')).toBeInTheDocument();
+  });
 });
-
-// App.test.jsx
-
-
-test('navigates to /vote', async () => {
-  const user = userEvent.setup();
-
-  //nechapu pro4 se to to tadz sekne na provideru
-  render(
-    <MemoryRouter initialEntries={['/']}>
-      <App useInternalRouter={false} />
-    </MemoryRouter>
-  );
-
-  await user.click(screen.getByTestId('app-bar'));
-  await user.click(screen.getByTestId('VOTE'));
-
-
-  expect(screen.getByText('SAVE IT!')).toBeInTheDocument();
-});
-
--- src/components/SideBar/Sidebar.test.jsx
+++ src/components/SideBar/Sidebar.test.jsx
@@ -5,40 +5,46 @@ import { MemoryRouter, Route, Routes } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import "@testing-library/jest-dom";
 
+describe('component SideBar', () => {
+  test("is visible and 3 button and picture of car", () => {
+    render(<MemoryRouter><Sidebar isOpen={true} /></MemoryRouter>);
 
+    expect(screen.getByTestId('side-bar')).toBeVisible();
+    expect(screen.getByTestId('cat')).toBeVisible();
+    expect(screen.getByTestId('VOTE')).toBeVisible();
+    expect(screen.getByTestId('BREEDS')).toBeVisible();
+    expect(screen.getByTestId('IMAGE/SEARCH')).toBeVisible();
+  });
 
+  test('toggleDrawer should be called after clicking on buttons', async () => {
+    const toggleDrawer = jest.fn();
+    const user = userEvent.setup();
 
+    render(
+      <MemoryRouter>
+        <Sidebar isOpen={true} toggleDrawer={toggleDrawer} />
+      </MemoryRouter>
+    );
 
+    // kliknu na tlačítko
+    await user.click(screen.getByTestId('VOTE'));
 
+    // it should close
+    expect(toggleDrawer).toHaveBeenCalledTimes(1);
+    // expect(screen.getAllByTestId("side-bar")).toBeVisible(false); proč tenhle test nefunguje když kliknu na button
+  });
 
+  test('is not rendered when isOpen=false', () => {
+    render(
+      <MemoryRouter>
+        <Sidebar isOpen={false} toggleDrawer={() => { }} />
+      </MemoryRouter>
+    );
 
-test("Sidebar is visible and 3 button and picture of car", () => {
-  render(<MemoryRouter><Sidebar isOpen={true} /></MemoryRouter>);
-
-  expect(screen.getByTestId('side-bar')).toBeVisible();
-  expect(screen.getByTestId('cat')).toBeVisible();
-  expect(screen.getByTestId('VOTE')).toBeVisible();
-  expect(screen.getByTestId('BREEDS')).toBeVisible();
-  expect(screen.getByTestId('IMAGE/SEARCH')).toBeVisible();
+    expect(screen.queryAllByTestId("side-bar")).toHaveLength(0);
+  });
 });
 
-test('toggleDrawer sjould be called after clicking on buttons', async () => {
-  const toggleDrawer = jest.fn();
-  const user = userEvent.setup();
-
-  render(
-    <MemoryRouter>
-      <Sidebar isOpen={true} toggleDrawer={toggleDrawer} />
-    </MemoryRouter>
-  );
-
-  // kliknu na tlačítko
-  await user.click(screen.getByTestId('VOTE'));
-
-  // it should close
-  expect(toggleDrawer).toHaveBeenCalledTimes(1);
-  // expect(screen.getAllByTestId("side-bar")).toBeVisible(false); proč tenhle test nefunguje
-});
 
 
 
